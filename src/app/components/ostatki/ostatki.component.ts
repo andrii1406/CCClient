367fd7be555fae2834653742c9eb911b26ac8b6e
andrii1406 <yuhim.andrey@gmail.com>
2023-07-17 14:44:19 +0300

    The visual interface for routing component 'Ostatki' was added.

--- src/app/components/ostatki/ostatki.component.ts
+++ src/app/components/ostatki/ostatki.component.ts
@@ -4,6 +4,9 @@ import {currency} from "../../model/currency";
 import {LoginParamsService} from "../../services/login-params/login-params.service";
 import {AuthService} from "../../services/jwt/auth.service";
 import {OstatkiService} from "../../services/ostatki.service";
+import {FormControl, FormGroup} from "@angular/forms";
+import {ppVlLocal} from "../../localdata/currencies";
+import {ostatkiLocal} from "../../localdata/ostatki";
 
 @Component({
   selector: 'app-ostatki',
@@ -11,6 +14,13 @@ import {OstatkiService} from "../../services/ostatki.service";
   styleUrls: ['./ostatki.component.scss']
 })
 export class OstatkiComponent {
+
+  ost: ostatki[] = ostatkiLocal
+  ppVl: currency[] = ppVlLocal
+
+  formListOst = new FormGroup({
+    ost: new FormControl<ostatki | null>(this.ost[0], []),
+  })
 
   constructor(
     private ostService: OstatkiService,
